6249c32c764883fb850f8ea3ce3b6d34abce0df7
AbdullahMufti <abdullahmufty@gmail.com>
2023-03-14 13:43:22 +0500

    added meta pic

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
     description: "This app was developedby Abdullah Mufti",
     images: [
       {
-        url: "/mypic.jpeg",
+        url: "https://pps.whatsapp.net/v/t61.24694-24/227763001_1021944651926586_291083306691608058_n.jpg?stp=dst-jpg_s96x96&ccb=11-4&oh=01_AdSml-Rk2mDWA7jUVkIqHX4CICCwz1Pu3PI732y-nFdmlQ&oe=641D1873",
         width: 200,
         height: 200,
       },
